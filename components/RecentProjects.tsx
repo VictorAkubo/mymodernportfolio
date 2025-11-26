96e51da20e53f93f5baf8941237db5e54d89f76b
VictorAkubo <138879202+VictorAkubo@users.noreply.github.com>
2025-11-26 05:04:34 +0000

    new update on height

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { projects } from "@/data";
 import { PinContainer } from "./ui/PinContainer";
 
@@ -12,64 +11,87 @@ const RecentProjects = () => {
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
+
       <div className="flex flex-wrap items-center justify-center p-4 gap-13 lg:gap-18 mt-10">
         {projects.map((item) => (
           <div
-            className="lg:min-h-[32.5rem] lg h-[28rem] flex items-center justify-center sm:w-96 w-[80vw]"
             key={item.id}
+            className="lg:min-h-[32.5rem] h-[28rem] flex items-center justify-center sm:w-96 w-[80vw]"
           >
-            <PinContainer
-              title={item.title}
-              href={item.link}
-            >
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+            <PinContainer title={item.title} href={item.link}>
+              
+              {/* IMAGE WRAPPER FIXED HEIGHT */}
+              <div
+                className="
+                  relative flex items-center justify-center
+                  sm:w-96 w-[80vw]
+                  overflow-hidden
+                  h-48 sm:h-56 lg:h-64
+                  mb-10
+                "
+              >
+                {/* BACKGROUND IMAGE*/}
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
                 >
-                  <img src="/bg.png" alt="bgimg" />
+                  <img
+                    src="/bg.png"
+                    alt="bgimg"
+                    className="w-full h-full object-cover"
+                  />
                 </div>
+
+                {/* PROJECT PREVIEW IMAGE */}
                 <img
-  src={item.img}
-  alt="cover"
-  className="z-10 absolute bottom-0 h-full max-h-full object-contain"
- />
+                  src={item.img}
+                  alt="cover"
+                  className="z-10 absolute bottom-0 h-full object-contain"
+                />
               </div>
 
+              {/* TITLE */}
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                 {item.title}
               </h1>
 
+              {/* DESCRIPTION */}
               <p
                 className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                style={{
-                  color: "#BEC1DD",
-                  margin: "1vh 0",
-                }}
+                style={{ color: "#BEC1DD", margin: "1vh 0" }}
               >
                 {item.des}
               </p>
 
+              {/* ICONS + LINK */}
               <div className="flex items-center justify-between mt-7 mb-3">
+
+                {/* STACK ICONS */}
                 <div className="flex items-center">
                   {item.iconLists.map((icon, index) => (
                     <div
                       key={index}
-                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      className="
+                        border border-white/[.2]
+                        rounded-full bg-black
+                        lg:w-10 lg:h-10 w-8 h-8
+                        flex justify-center items-center
+                      "
                       style={{
                         transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
-                      <img src={icon} alt="icon5" className="p-2" />
+                      <img src={icon} alt="icon" className="p-2" />
                     </div>
                   ))}
                 </div>
 
+                {/* LIVE SITE LINK */}
                 <div
                   className="flex justify-center items-center cursor-pointer"
                   onClick={(e) => {
-                    e.stopPropagation(); // prevent PinContainer click
-                    window.open(item.link, "_blank"); // open in new tab
+                    e.stopPropagation();
+                    window.open(item.link, "_blank");
                   }}
                 >
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
@@ -77,6 +99,7 @@ const RecentProjects = () => {
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
+
               </div>
             </PinContainer>
           </div>
